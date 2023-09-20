a98e217dccfd6d08a075f5ab8b03dcd09042de03
Pablo Bobarini <50304993+PablissimoB@users.noreply.github.com>
2023-09-20 16:39:00 -0300

    Agregar archivos de proyecto.

---
+++ Clase4/WebForm1.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="Clase4.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Button" />
+            <asp:Label ID="Label1" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:Label ID="Label2" runat="server"></asp:Label>
+            <br />
+            <br />
+            <asp:ListBox ID="ListBox1" runat="server" AutoPostBack="True" DataSourceID="SqlDataSource1" DataTextField="nombreCategoria" DataValueField="idCategoria" Height="186px" OnSelectedIndexChanged="ListBox1_SelectedIndexChanged"></asp:ListBox>
+            <br />
+            <asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+&nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Eliminar" />
+&nbsp;
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Modificar" />
+&nbsp;<asp:SqlDataSource ID="SqlDataSource1" runat="server" ConnectionString="<%$ ConnectionStrings:cadena %>" DeleteCommand="DELETE FROM [Categorias] WHERE [idCategoria] = @idCategoria" InsertCommand="INSERT INTO [Categorias] ([nombreCategoria]) VALUES (@nombreCategoria)" SelectCommand="SELECT * FROM [Categorias]" UpdateCommand="UPDATE [Categorias] SET [nombreCategoria] = @nombreCategoria WHERE [idCategoria] = @idCategoria">
+                <DeleteParameters>
+                    <asp:Parameter Name="idCategoria" Type="Int32" />
+                </DeleteParameters>
+                <InsertParameters>
+                    <asp:ControlParameter ControlID="TextBox1" Name="nombreCategoria" PropertyName="Text" Type="String" />
+                </InsertParameters>
+                <UpdateParameters>
+                    <asp:Parameter Name="nombreCategoria" Type="String" />
+                    <asp:Parameter Name="idCategoria" Type="Int32" />
+                </UpdateParameters>
+            </asp:SqlDataSource>
+            <asp:SqlDataSource ID="SqlDataSource2" runat="server" ConnectionString="<%$ ConnectionStrings:cadena %>" SelectCommand="SELECT * FROM [Categorias] WHERE ([idCategoria] = @idCategoria)">
+                <SelectParameters>
+                    <asp:ControlParameter ControlID="ListBox1" Name="idCategoria" PropertyName="SelectedValue" Type="Int32" />
+                </SelectParameters>
+            </asp:SqlDataSource>
+            <br />
+        </div>
+    </form>
+</body>
+</html>
---
+++ Clase4/WebForm1.aspx.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Clase4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                actualizarLabel();
+            }
+        }
+
+        protected void actualizarLabel()
+        {
+            Label2.Text = "";
+            DataView dv = (DataView) SqlDataSource1.Select(DataSourceSelectArguments.Empty);
+            foreach(DataRowView dr in dv)
+            {
+                DataRow row = dr.Row;
+                Label2.Text += row["nombreCategoria"].ToString() + " - ";
+            }
+
+        }
+
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            //SqlDataSource1.InsertParameters["idCategoria"].DefaultValue = ListBox1.SelectedValue.ToString();
+
+            int result = SqlDataSource1.Insert();
+            if (result != 0)
+            {
+                Label1.Text = "Se ha agregado "+result.ToString() + " registros";
+                actualizarLabel();
+                TextBox1.Text = "";
+            }
+            else
+            {
+                Label1.Text = " Noo se agregaron registros";
+            }
+        }
+
+        protected void ListBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlDataSource2.DataSourceMode = SqlDataSourceMode.DataReader;
+            SqlDataReader reader = (SqlDataReader)SqlDataSource2.Select(DataSourceSelectArguments.Empty);
+            if (reader.Read())
+            {
+                TextBox2.Text = reader["nombreCategoria"].ToString();
+            }
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.DeleteParameters["idCategoria"].DefaultValue = ListBox1.SelectedValue.ToString();
+            int result = SqlDataSource1.Delete();
+            if (result != 0)
+            {
+                Label1.Text = "Se ha eliminado " + result.ToString() + " registros";
+                actualizarLabel();
+                TextBox2.Text = "";
+            }
+            else
+            {
+                Label1.Text = " No se eliminaron los elementos.";
+            }
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            SqlDataSource1.UpdateParameters["idCategoria"].DefaultValue = ListBox1.SelectedValue.ToString();
+            SqlDataSource1.UpdateParameters["nombreCategoria"].DefaultValue = TextBox2.Text;
+            int result = SqlDataSource1.Update();
+            if (result != 0)
+            {
+                Label1.Text = "Se ha modificado " + result.ToString() + " registros";
+                actualizarLabel();
+                TextBox2.Text = "";
+            }
+            else
+            {
+                Label1.Text = " No se modificado los elementos.";
+            }
+
+        }
+    }
+}
